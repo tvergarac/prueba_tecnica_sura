d03aecce4173758f6abf02126a6d0b13ad101675
tvergarac <68257810+tvergarac@users.noreply.github.com>
2020-10-15 19:26:25 -0500

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 17:44:51 2020
+
+@author: tomvc
+"""
+
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+from sodapy import Socrata
+
+def get_TRM():
+    client = Socrata("www.datos.gov.co", None)
+    w = "vigenciadesde >= '{}'".format(dt.datetime.strftime(dt.date(2008, 1, 1), '%Y-%m-%d'))
+    r = client.get("32sa-8pi3", where=w, limit=10000000)
+    df = pd.DataFrame.from_records(r)
+    df = df[['vigenciadesde', 'valor']]
+    df.columns = ['Fecha', 'TRM']
+    df['Fecha'] = pd.to_datetime(df['Fecha'])
+    df['Fecha'] = [i - dt.timedelta(days=1) for i in df['Fecha']]
+    df['TRM'] = pd.to_numeric(df['TRM'])
+    df.sort_values('Fecha', inplace=True)
+    return df
+
+def get_data(ticker, fecha_inicial, fecha_final):
+    df = yf.download(ticker, 
+                       start=dt.datetime.strftime(fecha_inicial, '%Y-%m-%d'),
+                       end=dt.datetime.strftime(fecha_final, '%Y-%m-%d'),
+                       progress=False)
+    df.reset_index(level=0, inplace=True)
+    return df[['Date', 'Close', 'Open', 'High', 'Low']]
