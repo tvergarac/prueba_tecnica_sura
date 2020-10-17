940252b2406d32c4ac6e39b0b40a78404cdf8f8b
tvergarac <68257810+tvergarac@users.noreply.github.com>
2020-10-16 21:00:01 -0500

    D2

--- app.py
+++ app.py
@@ -11,26 +11,53 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import datetime as dt
-#import pandas as pd
-#import dash_table
-#import plotly.graph_objects as go
+import pandas as pd
+import dash_table
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+import plotly.express as px
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 suppress_callback_exceptions = True
 
-from funciones import ultDiaHabil, get_TRM, get_data
+from funciones import ultDiaHabil, get_TRM, get_data, get_TRM_BoxPlot, trm_table_yr
 
-graf_trm = dbc.Card([
+trm_oil_1 = dbc.Card([
+                dcc.Graph(id='graf')
+                ])
+
+trm_box = dbc.Card([
+        dcc.Graph(id='trm-box')
+        ])
+
+corr = dbc.Card([
         dbc.CardBody([
-                html.H6('TRM', className='card-title'),
-                dcc.Graph(id='trm')
+                html.H4('Correlación TRM vs Petróleo'),
+                html.H6('Fecha desde: '),
+                dcc.DatePickerSingle(id='date-corr',
+                                    date=ultDiaHabil() - dt.timedelta(days=365),
+                                    min_date_allowed=dt.date(2009, 1, 1),
+                                    max_date_allowed=ultDiaHabil(),
+                                    display_format = 'D/M/Y',
+                                    persistence=False),
+                dcc.Graph(id='corr')
                 ])
         ])
 
-graf_oil = dbc.Card([
+trm_table = dbc.Card([
         dbc.CardBody([
-                html.H6('Petróleo', className='card-title'),
-                dcc.Graph(id='oil')
+                html.H4('Información TRM Anual'),
+                dash_table.DataTable(id='trm-table',
+                                     merge_duplicate_headers=True,
+                                     style_table={'overflowX' : 'auto'},
+                                     style_data_conditional=[
+                                             {'if' : {'row_index' : 'odd'},
+                                              'backgroundColor' : 'rgb(248, 248, 248)'}],
+                                     style_header={'backgroundColor' : 'rgb(230, 230, 230)',
+                                                   'fontWeight' : 'bold'},
+                                     style_cell={'textAlign' : 'center'}
+                                             )
                 ])
         ])
 
@@ -54,9 +81,17 @@ trm_oil = html.Div([
                 ], justify='start'),
         dbc.Row(dbc.Col(html.Div(html.Br()))),
         dbc.Row([
-                dbc.Col(graf_trm, width={'size' : 5, 'offset' : 1}),
-                dbc.Col(graf_oil, width=5)
-                ])
+                dbc.Col(trm_oil_1, width={'size' : 10, 'offset' : 1}),
+                ]),
+        dbc.Row(dbc.Col(html.Div(html.Br()))),
+        dbc.Row([
+                dbc.Col(trm_box, width={'size' : 10, 'offset' : 1}),
+                ]),
+        dbc.Row(dbc.Col(html.Div(html.Br()))),
+        dbc.Row([
+                dbc.Col(corr, width={'size' : 5, 'offset' : 1}),
+                dbc.Col(trm_table, width=5)
+                ]),
         ])
 
 spy = html.Div(html.H1('Hola'))
@@ -78,6 +113,120 @@ layout = html.Div([
 
 app.layout = layout
 
+#Grafica TRM y Petroleo - Grafica Correlacion
+
+def fig_trm_oil(df, freq):
+    fig = make_subplots(specs=[[{'secondary_y' : True}]])
+    
+    fig.add_trace(
+            go.Scatter(
+                    x = df.index,
+                    y = df.TRM,
+                    name = 'TRM ' + freq),
+            secondary_y = False
+                    )
+    
+    fig.add_trace(
+            go.Scatter(
+                    x = df.index,
+                    y = df.OIL,
+                    name = 'Petróleo ' + freq),
+            secondary_y = True
+                    )
+    
+    fig.update_layout(title_text = 'Evolución {} TRM y Petróleo'.format(freq),
+                      yaxis_tickformat = '$')
+    
+    fig.update_xaxes(title_text='Fecha')
+    
+    fig.update_yaxes(title_text='<b>TRM (COP)</b>',secondary_y=False)
+    fig.update_yaxes(title_text='<b>Petróleo($USD/Barril)</b>', tickformat='$', secondary_y=True)
+    return fig
+
+@app.callback([Output('graf', 'figure'), Output('corr', 'figure')],
+               [Input('fecha_corte', 'date'), Input('freq', 'value'), Input('date-corr', 'date')])
+
+def graf_trm_oil(fecha, freq, fi):
+    if fecha is not None:
+        fecha = dt.datetime(int(fecha[:4]), int(fecha[5:7]), int(fecha[8:10]), 0, 0, 0)
+    if fi is not None:
+        fi = dt.datetime(int(fi[:4]), int(fi[5:7]), int(fi[8:10]), 0, 0, 0)
+    #Traer Data   
+    df_trm = get_TRM(fecha)
+    df_oil = get_data('CL=F', dt.datetime(2008, 1, 1, 0, 0, 0), fecha)
+    df_oil = df_oil.loc[:, ['Date', 'Close']]
+    df_oil.columns = ['Fecha', 'OIL']
+    df = pd.merge(df_trm, df_oil, how='left')
+    df.fillna(method='bfill', inplace=True)
+    df.set_index('Fecha', inplace=True)
+    
+    #Grafica principal
+    if freq == 'Mensual':
+        dfm = df.sort_index().resample('M').apply(lambda x: x.iloc[-1,])
+        i = dfm.index.tolist()
+        i[-1] = fecha
+        dfm.index = i
+        fig = fig_trm_oil(dfm, freq)
+    elif freq == 'Anual':
+        dfy = df.sort_index().resample('Y').apply(lambda x: x.iloc[-1,])
+        i = dfy.index.tolist()
+        i[-1] = fecha
+        dfy.index = i
+        fig = fig_trm_oil(dfy, freq)
+    else:
+        fig = fig_trm_oil(df, freq)
+    
+    #Grafica Correlacion
+    df = df[df.index >= fi]
+    
+    df['rTRM'] = np.log(df['TRM'] / df['TRM'].shift(1))
+    df['rOIL'] = np.log(df['OIL'] / df['OIL'].shift(1))
+    
+    c = df['rTRM'].corr(df['rOIL'])
+    
+    fig2 = px.scatter(
+            x=df['rTRM'],
+            y=df['rOIL'],            
+            trendline='ols',
+            trendline_color_override='red'
+            )
+    
+    fig2.update_layout(title_text = 'Correlación TRM vs Petróleo: {:.3}'.format(c))
+    fig2.update_xaxes(title_text='<b>TRM</b>', tickformat='%')
+    fig2.update_yaxes(title_text='<b>Petróleo</b>',tickformat='%')
+    
+    return fig, fig2
+
+#Boxplot TRM Mensual
+
+@app.callback(Output('trm-box', 'figure'), [Input('fecha_corte', 'date')])
+
+def trm_boxplot(fecha):
+    df = get_TRM_BoxPlot(fecha)
+    ma = list(df['MA'].unique())
+    fig = go.Figure(data=[
+            go.Box(
+                    y = df[df['MA'] == i]['TRM'],
+                    name=str(i)
+                    ) for i in ma
+            ])
+    fig.update_layout(
+            title_text='Boxplot Mensual TRM desde 2010',
+            showlegend=False
+            )
+    fig.update_yaxes(title_text='<b>TRM (COP)</b>', tickformat='$')
+    return fig
+
+#Tabla con el Resumen Anual de TRM
+
+@app.callback([Output('trm-table', 'data'), Output('trm-table', 'columns')],
+               [Input('fecha_corte', 'date')])
+
+def trm_year_table(fecha):
+    if fecha is not None:
+        fecha = dt.datetime(int(fecha[:4]), int(fecha[5:7]), int(fecha[8:10]), 0, 0, 0)
+    df = trm_table_yr(fecha)
+    return df.to_dict('rows'), [{'name' : i, 'id' : i} for i in df.columns]
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0',debug=True, port=8050)
--- funciones.py
+++ funciones.py
@@ -6,12 +6,15 @@ Created on Thu Oct 15 17:44:51 2020
 """
 
 import pandas as pd
+import numpy as np
 import yfinance as yf
 import datetime as dt
 from sodapy import Socrata
 import holidays
+import warnings
+warnings.filterwarnings('ignore')
 
-def get_TRM():
+def get_TRM(fecha):
     client = Socrata("www.datos.gov.co", None)
     w = "vigenciadesde >= '{}'".format(dt.datetime.strftime(dt.date(2008, 1, 1), '%Y-%m-%d'))
     r = client.get("32sa-8pi3", where=w, limit=10000000)
@@ -19,6 +22,7 @@ def get_TRM():
     df = df[['vigenciadesde', 'valor']]
     df.columns = ['Fecha', 'TRM']
     df['Fecha'] = pd.to_datetime(df['Fecha'])
+    df = df[df.Fecha <= fecha]
     df['Fecha'] = [i - dt.timedelta(days=1) for i in df['Fecha']]
     df['TRM'] = pd.to_numeric(df['TRM'])
     df.sort_values('Fecha', inplace=True)
@@ -30,7 +34,7 @@ def get_data(ticker, fecha_inicial, fecha_final):
                        end=dt.datetime.strftime(fecha_final, '%Y-%m-%d'),
                        progress=False)
     df.reset_index(level=0, inplace=True)
-    return df[['Date', 'Close', 'Open', 'High', 'Low']]
+    return df
 
 def ultDiaHabil():
     f = dt.date.today() - dt.timedelta(days=1)
@@ -41,4 +45,21 @@ def ultDiaHabil():
             break
     return f
 
-f = ultDiaHabil()
+def get_TRM_BoxPlot(fecha):
+    df = get_TRM(fecha)
+    df['MA'] = pd.to_datetime(df['Fecha']).dt.to_period('M')
+    df = df[df.Fecha > dt.datetime(2009, 12, 31, 0, 0, 0)]
+    return df
+
+def trm_table_yr(fecha):
+    df = get_TRM(fecha)
+    df.set_index('Fecha', inplace=True)
+    df = df.groupby(pd.Grouper(freq='Y')).agg(
+            {'TRM' : ['min', 'max', 'mean', np.std]}
+            ).dropna()
+    for col in df.columns:
+        df[col] = df[col].apply(lambda x: '${:0,.2f}'.format(x))
+    df.reset_index(level=0, inplace=True)
+    df['Fecha'] = [i.year for i in df['Fecha']]
+    df.columns=['Fecha', 'Mínimo', 'Máximo', 'Media', 'Std_Dev']
+    return df
